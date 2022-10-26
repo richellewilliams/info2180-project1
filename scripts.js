e34d6f052d6ab3dc73722505e35ba719585a525c
Richelle Williams <richellewilliams3@gmail.com>
2022-10-26 10:03:01 -0500

    Added JavaScript code to the Newsletter Email Subscription

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-/* Add your JavaScript to this file */
+"use strict";
+
+window.onload = function() {
+    const email = document.getElementById("email");
+    const form = document.querySelector("form");
+    const message = document.querySelector(".message");
+    
+    function emailInputCheck() {
+        if (email.value != "") {
+            message.textContent = "Thank you! Your email address", email.value, "has been added to our mailing list!";
+        } else {
+            message.textContent = "Please enter a valid email address";
+        }
+    }
+    
+    form.addEventListener('submit',emailInputCheck);
+}
