00bb5a784feb30035e904f0a60c87cb9eed8951a
Richelle Williams <richellewilliams3@gmail.com>
2022-10-27 00:46:54 -0500

    Updated the JavaScript code to properly display the entered email address

--- scripts.js
+++ scripts.js
@@ -7,9 +7,9 @@ window.onload = function() {
     
     function emailInputCheck() {
         if (email.value != "") {
-            message.textContent = "Thank you! Your email address", email.value, "has been added to our mailing list!";
+            message.textContent = "Thank you! Your email address " + email.value + " has been added to our mailing list!";
         } else {
-            message.textContent = "Please enter a valid email address";
+            message.textContent = "Please enter a valid email address.";
         }
     }
     
